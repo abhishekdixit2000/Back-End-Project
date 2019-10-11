f9466e29be3923ac36f2989f7cc8f63a0dc59b94
abhishekdixit2000 <49614061+abhishekdixit2000@users.noreply.github.com>
2019-10-11 22:22:17 +0530

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Planner Pros</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/gijgo.min.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/all.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="main_menu_iner">
+                        <div class="search_icon">
+                            <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
+                        </div>
+                        <div class="logo">
+                            <a href="visiting card.php"><h1 style="color:white;">PLANNER <span style="color: orange">PROS</span></h1></a>
+                        </div>
+                        <span class="menu-trigger visible-xs">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                        <div class="off-canven-menu">
+                            <span class="close-icon">
+                                <i class="ti-close"></i>
+                            </span>
+                            <div class="canven-menu-warp">
+                                <div class="canven-menu-iner">
+                                    <ul>
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php">About Us</a></li>
+                                        <li><a href="service.php">Services</a></li>
+                                        <li><a href="photogallery.php">Photogallery</a></li>
+                                        <li><a href="#contact">Contact</a></li>
+                                        <li class="nav-item cta cta-colored"><a href="adminlogin.php" class="nav-link"><span class="glyphicon glyphicon-shopping-cart"></span>Admin Login</a></li>
+                                        <li><a href="userlogin.php">User Login</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="search_input" id="search_input_box">
+            <div class="container ">
+                <form class="d-flex justify-content-between search-inner">
+                    <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+                    <button type="submit" class="btn"></button>
+                    <span class="ti-close" id="close_search" title="Close Search"></span>
+                </form>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
